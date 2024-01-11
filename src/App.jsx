849967080cb4b1ea8a99267b24720ab96b3e6f9e
indrajith76 <indrajithgoswami@gmail.com>
2024-01-11 16:03:05 +0600

    update info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Indrajith Goswami",
+  title: "Front End Developer & Tech Enthusiast",
+  email: "Indrajith.Goswami@studentambassadors.com",
+  gitHub: "indrajith76",
+  instagram: "#",
+  linkedIn: "indrajithgoswami",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "indrajithcse",
+  youTube: "UCm9IQE-aF3NzxEcfWDmF0oA",
 };
 
 const primaryColor = "#4E567E";
